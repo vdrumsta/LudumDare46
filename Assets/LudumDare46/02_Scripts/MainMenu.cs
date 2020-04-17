49ded074debf2dc9c9ae7959d3aa8b229cc297e0
Vilius Drumsta <vdrumsta@gmail.com>
2020-04-17 23:45:28 +0100

    Added a menu

---
+++ Assets/LudumDare46/02_Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool _displayingHelp;
+    private GameObject _helpObject;
+
+    public void DisplayHelp(GameObject helpImageObject)
+    {
+        _helpObject = helpImageObject;
+        _helpObject.SetActive(true);
+        StartCoroutine(DelayReadyToHide(1));
+    }
+
+    public void Update()
+    {
+        if (_displayingHelp && Input.anyKeyDown)
+        {
+            _helpObject.SetActive(false);
+            _displayingHelp = false;
+        }
+    }
+
+    IEnumerator DelayReadyToHide(float seconds)
+    {
+        yield return new WaitForSeconds(1);
+        _displayingHelp = true;
+    }
+}
---
+++ Assets/LudumDare46/02_Scripts/SceneHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHelper : MonoBehaviour
+{
+    public string sceneName;
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByPublicReference()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
