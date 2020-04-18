ee5a250f94529bc5eb7866cc77c9480426d3a9ac
Vilius Drumsta <vdrumsta@gmail.com>
2020-04-18 23:21:57 +0100

    Add immunity flashing

--- Assets/LudumDare46/02_Scripts/PlayerHealth.cs
+++ Assets/LudumDare46/02_Scripts/PlayerHealth.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,12 @@ public class PlayerHealth : MonoBehaviour
     // When collide with this layer, player loses health
     public LayerMask damageObjectLayerMask;
     public List<GameObject> healthHearts;
-    public int currentHearts;
+    [HideInInspector] public int currentHearts;
+
+    public bool immune;
+    public List<Material> playerMaterials;
+    public float flashInterval = 0.1f;
+
     private int maxHealth;
 
     // Start is called before the first frame update
@@ -15,6 +21,7 @@ public class PlayerHealth : MonoBehaviour
     {
         maxHealth = healthHearts.Count;
         currentHearts = healthHearts.Count;
+        GhostPlayerMaterials(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,14 +43,52 @@ public class PlayerHealth : MonoBehaviour
 
     private void DecreaseHealth()
     {
-        if (currentHearts <= 0) return;
+        if (currentHearts <= 0 || immune) return;
 
         healthHearts[currentHearts - 1].SetActive(false);
         currentHearts--;
 
+        immune = true;
+        StartCoroutine(RemoveImmunityAfterTime(1));
+
         if (currentHearts <= 0)
         {
             // Die();
+        }
+    }
+
+    IEnumerator RemoveImmunityAfterTime(float seconds)
+    {
+        float timeCounter = 0;
+
+        while (timeCounter < seconds)
+        {
+            GhostPlayerMaterials(true);
+            yield return new WaitForSeconds(flashInterval);
+            GhostPlayerMaterials(false);
+            yield return new WaitForSeconds(flashInterval);
+
+            timeCounter += flashInterval * 2;
+        }
+
+        immune = false;
+    }
+
+    private void GhostPlayerMaterials(bool transparent)
+    {
+        foreach(var material in playerMaterials)
+        {
+            var currentColor = material.color;
+
+            if (transparent)
+            {
+                currentColor.a = 0.5f;
+            }
+            else
+            {
+                currentColor.a = 1f;
+            }
+            material.color = currentColor;
         }
     }
 }
