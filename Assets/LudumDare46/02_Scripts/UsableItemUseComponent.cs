22027bb9d9dc2d0c61465a5ebbd9d458cb01ff50
Matthew Murphy <hellomatthewmurphy@gmail.com>
2020-04-19 17:14:48 +0200

    Added watering the plant

---
+++ Assets/LudumDare46/02_Scripts/UsableItemUseComponent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsableItemUseComponent : MonoBehaviour
+{
+    private UsableItem usableItem;
+    private Coroutine addingToPlantCoroutine;
+    PlantController currentPlant;
+
+    private void Start()
+    {
+        usableItem = this.GetComponentInParent<UsableItem>();
+        usableItem.onStartDelegate += StartUsing;
+        usableItem.onStopDelegate += Stopusing;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlantController plant = other.GetComponent<PlantController>();
+        if (plant)
+        {
+            currentPlant = plant;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlantController plant = other.GetComponent<PlantController>();
+        if (plant)
+        {
+            currentPlant = null;
+        }
+    }
+
+    private void StartUsing()
+    {
+        if (currentPlant)
+        {
+            addingToPlantCoroutine = StartCoroutine(AddToPlantRoutine(currentPlant));
+        }
+    }
+
+    private void Stopusing()
+    {
+        if (addingToPlantCoroutine != null)
+        {
+            StopCoroutine(addingToPlantCoroutine);
+            addingToPlantCoroutine = null;
+        }
+    }
+
+    private IEnumerator AddToPlantRoutine(PlantController plant)
+    {
+        do
+        {
+            yield return new WaitForSeconds(usableItem.useSpeed);
+            plant.AddStat(usableItem.statTypeForPlant, 1);
+
+        } while (true);
+    }
+}
