ff2be612a1788d4f99f50349cba8fbb834890e1c
Matthew Murphy <hellomatthewmurphy@gmail.com>
2020-04-20 11:10:29 +0200

    Added new art and animations

--- Assets/LudumDare46/02_Scripts/Player.cs
+++ Assets/LudumDare46/02_Scripts/Player.cs
@@ -10,12 +10,24 @@ public class Player : MonoBehaviour
     private Vector3 velocityVector;
     private PlayerFeet feet;
     private PlayerArms arms;
+    private Animator characterAnimator;
+
+    private int isWalking_hash = Animator.StringToHash("IsWalking");
+    private int pickUp_hash = Animator.StringToHash("PickUP");
+    private int drop_hash = Animator.StringToHash("Drop");
+    private int die_hash = Animator.StringToHash("Die");
+
+    public bool isWalkingAnimation { get { return characterAnimator.GetBool(isWalking_hash); } set { characterAnimator.SetBool(isWalking_hash, value); } }
+    public bool pickUpAnimation { get { return characterAnimator.GetBool(pickUp_hash); } set { characterAnimator.SetBool(pickUp_hash, value); } }
+    public bool dropAnimation { get { return characterAnimator.GetBool(drop_hash); } set { characterAnimator.SetBool(drop_hash, value); } }
+    public bool dieAnimation { get { return characterAnimator.GetBool(die_hash); } set { characterAnimator.SetBool(die_hash, value); } }
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         feet = GetComponentInChildren<PlayerFeet>();
         arms = GetComponentInChildren<PlayerArms>();
+        characterAnimator = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -37,7 +49,12 @@ public class Player : MonoBehaviour
 
         if (moveVector != Vector3.zero)
         {
+            isWalkingAnimation = true;
             transform.forward = moveVector;
+        }
+        else if(isWalkingAnimation)
+        {
+            isWalkingAnimation = false;
         }
     }
 }
